b0d380aef245b74b046aa2147d6112e2cc08bf4a
Daniel J. Pritchett <dpritchett@gmail.com>
2017-01-26 17:08:52 -0600

    read me

---
+++ README.md
@@ -0,0 +1,7 @@
+# How to run me
+`ruby app.rb`
+
+## TODO:
+- Reconnect if hubot restarts
+- Install on mac mini
+- Respond to hubot if I fail to relay a song request to Sonos
--- app.rb
+++ app.rb
@@ -7,12 +7,17 @@ SERVICE_URL = 'http://clearbot.herokuapp.com'
 
 socket = SocketIO::Client::Simple.connect SERVICE_URL
 
+def log(msg)
+  puts "** #{msg}"
+end
+
 socket.on :connect do
-  puts "connected!!!"
+  log "Connected to server: #{SERVICE_URL}"
 end
 
 socket.on :disconnect do
-  puts "disconnected!!"
+  log "Lost contact with server: #{SERVICE_URL}"
+  log "I don't know how to reconnect yet.  Please help!"
 end
 
 def play_url(url)
@@ -24,7 +29,7 @@ socket.on :play_url do |data|
   url = data["url"]
 
   if url.nil?
-    puts "Unusable payload: #{data}"
+    log "Unusable payload: #{data}"
   else
     play_url url
   end
@@ -34,7 +39,7 @@ socket.on :error do |err|
   p err
 end
 
-puts "Please input and press Enter key"
+puts "I'm gonna sit and wait for keyboard input forever now... feel free to ignore me."
 
 loop do
   begin
@@ -43,6 +48,6 @@ loop do
     socket.emit :chat, {:msg => msg, :at => Time.now}
   rescue Interrupt
     socket.disconnect
-    abort "goodbye!"
+    abort "Goodbye!"
   end
 end
