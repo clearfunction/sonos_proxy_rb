e19b7fed8429958d3033dfd707cd4ce59988f9d6
Daniel J. Pritchett <dpritchett@gmail.com>
2017-02-10 15:07:36 -0600

    Kinda sorta reconnects sometimes

--- app.rb
+++ app.rb
@@ -1,53 +1,28 @@
-require 'rubygems'
-require 'socket.io-client-simple'
+require_relative './listener'
 
-require_relative './burn'
-
-SERVICE_URL = 'http://clearbot.herokuapp.com'
-
-socket = SocketIO::Client::Simple.connect SERVICE_URL
-
-def log(msg)
-  puts "** #{msg}"
+def reload!
+  puts "** Setting up a new listener..."
+  @listener = Listener.new
 end
 
-socket.on :connect do
-  log "Connected to server: #{SERVICE_URL}"
-end
+def main
+  reload!
 
-socket.on :disconnect do
-  log "Lost contact with server: #{SERVICE_URL}"
-  log "I don't know how to reconnect yet.  Please help!"
-end
+  puts "I'm gonna sit and wait for keyboard input forever now... feel free to ignore me."
 
-def play_url(url)
-  base_burn = ['https://www.dropbox.com/s/ql3z6d7kmoyof99/sick_burn.mp3?dl=1'].sample
-  Burn.burn!(url)
-end
+  loop do
+    begin
+      sleep 1
+      
+      reload! if @listener.dead?
+    rescue SocketDroppedError, Errno::ECONNRESET
+      reload!
+    rescue Interrupt
+      @listener.disconnect
 
-socket.on :play_url do |data|
-  url = data["url"]
-
-  if url.nil?
-    log "Unusable payload: #{data}"
-  else
-    play_url url
+      abort "Goodbye!"
+    end
   end
 end
 
-socket.on :error do |err|
-  p err
-end
-
-puts "I'm gonna sit and wait for keyboard input forever now... feel free to ignore me."
-
-loop do
-  begin
-    msg = STDIN.gets.strip
-    next if msg.empty?
-    socket.emit :chat, {:msg => msg, :at => Time.now}
-  rescue Interrupt
-    socket.disconnect
-    abort "Goodbye!"
-  end
-end
+main
---
+++ listener.rb
@@ -0,0 +1,60 @@
+require 'rubygems'
+require 'socket.io-client-simple'
+
+require_relative './burn'
+require_relative './listener'
+
+class SocketDroppedError< StandardError; end
+
+class Listener
+  attr_reader :socket
+  def initialize
+    @socket = SocketIO::Client::Simple.connect(SERVICE_URL,
+      ping_interval: 5000, ping_timeout: 1000)
+    instrument_socket
+    socket
+  end
+
+  def dead?
+    open = socket.open?
+    emitted = socket.emit('health_check', 'hi')
+    false
+  end
+  
+  SERVICE_URL = 'http://clearbot.herokuapp.com'
+
+  def play_url(url)
+    base_burn = ['https://www.dropbox.com/s/ql3z6d7kmoyof99/sick_burn.mp3?dl=1'].sample
+    Burn.burn!(url)
+  end
+
+  def instrument_socket
+    socket.on :connect do
+      puts "Connected to server: #{SERVICE_URL}"
+    end
+
+    socket.on :disconnect do
+      puts "Lost contact with server: #{SERVICE_URL}"
+      puts "I don't know how to reconnect yet.  Please help!"
+      raise SocketDroppedError
+    end
+
+    socket.on :play_url do |data|
+      url = data["url"]
+
+      if url.nil?
+        puts "Unusable payload: #{data}"
+      else
+        play_url url
+      end
+    end
+
+    socket.on :error do |err|
+      p err
+    end
+  end
+
+  def disconnect
+    socket.disconnect
+  end
+end
